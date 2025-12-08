550eaf4b93c1c0447d8c89f3af061f8438c36a4e
lordFella07 <azharuddindedeq@gmail.com>
2025-12-08 22:26:37 +0800

    Example of login page

---
+++ login/login.js
@@ -0,0 +1,9 @@
+// Show/Hide password
+
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput   = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+    const type = passwordInput.type === "password" ? "text" : "password";
+    passwordInput.type = type;
+});
